3ac7a351ef7d00e7840acb5a28138a7bfc431a5c
GDA0 <gideondelaliadeti0@gmail.com>
2024-06-15 17:36:25 +0000

    Improve navigation on mobile

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const form = document.querySelector(".needs-validation");
 
 form.addEventListener(
-  "submit",
-  (event) => {
-    if (!form.checkValidity()) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+	"submit",
+	(event) => {
+		if (!form.checkValidity()) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
 
-    form.classList.add("was-validated");
-  },
-  false
+		form.classList.add("was-validated");
+	},
+	false
 );
+
+const navbarCollapse = document.querySelector(".navbar-collapse");
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+			toggle: false,
+		});
+		bsCollapse.hide();
+	});
+});
